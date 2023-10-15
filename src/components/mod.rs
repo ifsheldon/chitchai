d8a2217a74b3ab7ad13529dac7d12c913970f917
Feng Liang <feng.liang@kaust.edu.sa>
2023-10-15 11:31:58 +0800

    remove unused buttons for now

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -68,102 +68,11 @@ pub fn PromptMessage(cx: Scope, msg: String, left: bool) -> Element {
                         "{msg}"
                     }
                 }
-                div {
-                    class: "mr-2 mt-1 flex flex-col-reverse gap-2 text-slate-500 sm:flex-row",
-                    GoodButton {},
-                    BadButton {},
-                    CopyButton {},
-                }
             }
         }
     }
 }
 
-
-pub fn GoodButton(cx: Scope) -> Element {
-    render! {
-        button {
-            class: "hover:text-blue-600",
-            r#type: "button",
-            svg {
-                xmlns: "http://www.w3.org/2000/svg",
-                class: "h-5 w-5",
-                view_box: "0 0 24 24",
-                stroke_width: "2",
-                stroke: "currentColor",
-                fill: "none",
-                stroke_linecap: "round",
-                stroke_linejoin: "round",
-                path {
-                    stroke: "none",
-                    d: "M0 0h24v24H0z",
-                    fill: "none"
-                }
-                path {
-                    d: "M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"
-                }
-                path {
-                    d: "M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"
-                }
-            }
-        }
-    }
-}
-
-
-pub fn BadButton(cx: Scope) -> Element {
-    render! {
-        button {
-            class: "hover:text-blue-600",
-            r#type: "button",
-            svg {
-                xmlns: "http://www.w3.org/2000/svg",
-                class: "h-5 w-5",
-                view_box: "0 0 24 24",
-                stroke_width: "2",
-                stroke: "currentColor",
-                fill: "none",
-                stroke_linecap: "round",
-                stroke_linejoin: "round",
-                path {
-                    stroke: "none",
-                    d: "M0 0h24v24H0z",
-                    fill: "none"
-                }
-                path {
-                    d: "M7 13v-8a1 1 0 0 0 -1 -1h-2a1 1 0 0 0 -1 1v7a1 1 0 0 0 1 1h3a4 4 0 0 1 4 4v1a2 2 0 0 0 4 0v-5h3a2 2 0 0 0 2 -2l-1 -5a2 3 0 0 0 -2 -2h-7a3 3 0 0 0 -3 3"
-                }
-            }
-        }
-    }
-}
-
-pub fn CopyButton(cx: Scope) -> Element {
-    render! {
-        div {
-            button {
-                svg {
-                    xmlns: "http://www.w3.org/2000/svg",
-                    class: "h-5 w-5",
-                    view_box: "0 0 24 24",
-                    stroke_width: "2",
-                    stroke: "currentColor",
-                    fill: "none",
-                    stroke_linecap: "round",
-                    stroke_linejoin: "round",
-                    path {
-                        stroke: "none",
-                        d: "M0 0h24v24H0z",
-                        fill: "none"
-                    }
-                    path {
-                        d: "M7 11v8a1 1 0 0 1 -1 1h-2a1 1 0 0 1 -1 -1v-7a1 1 0 0 1 1 -1h3a4 4 0 0 0 4 -4v-1a2 2 0 0 1 4 0v5h3a2 2 0 0 1 2 2l-1 5a2 3 0 0 1 -2 2h-7a3 3 0 0 1 -3 -3"
-                    }
-                }
-            }
-        }
-    }
-}
 
 pub fn PromptMessageInput(cx: Scope) -> Element {
     render! {
