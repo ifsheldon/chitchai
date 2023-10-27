cace5dbd76589c4bb200a8fbcfc2a9ac45727f4c
Feng Liang <feng.liang@kaust.edu.sa>
2023-10-27 21:05:28 +0800

    make msg icon based on name

--- src/components/chat/message_card.rs
+++ src/components/chat/message_card.rs
@@ -26,32 +26,40 @@ pub fn MessageCard(cx: Scope<MessageCardProps>) -> Element {
                     }
                 }
             },
-        Role::User => render! {
-            div {
-                class: "flex flex-row-reverse items-start p-5",
-                img {
-                    class: "ml-2 h-8 w-8 rounded-full",
-                    src: "https://dummyimage.com/128x128/354ea1/ffffff&text=U"
-                }
+        Role::User => {
+            let name = chat_msg.msg.name.as_ref();
+            let char = name.map(|name| name.chars().next().unwrap()).unwrap_or('U');
+            render! {
                 div {
-                    class: "flex min-h-[85px] rounded-b-xl rounded-tl-xl bg-slate-50 p-4 dark:bg-slate-800 sm:min-h-0 sm:max-w-md md:max-w-2xl",
-                    p {
-                        "{msg}"
+                    class: "flex flex-row-reverse items-start p-5",
+                    img {
+                        class: "ml-2 h-8 w-8 rounded-full",
+                        src: "https://dummyimage.com/128x128/354ea1/ffffff&text={char}"
+                    }
+                    div {
+                        class: "flex min-h-[85px] rounded-b-xl rounded-tl-xl bg-slate-50 p-4 dark:bg-slate-800 sm:min-h-0 sm:max-w-md md:max-w-2xl",
+                        p {
+                            "{msg}"
+                        }
                     }
                 }
             }
         },
-        Role::Assistant => render! {
-            div {
-                class: "flex items-start p-5",
-                img {
-                    class: "mr-2 h-8 w-8 rounded-full",
-                    src: "https://dummyimage.com/128x128/363536/ffffff&text=A"
-                }
+        Role::Assistant => {
+            let name = chat_msg.msg.name.as_ref();
+            let char = name.map(|name| name.chars().next().unwrap()).unwrap_or('A');
+            render! {
                 div {
-                    class: "flex rounded-b-xl rounded-tr-xl bg-slate-50 p-4 dark:bg-slate-800 sm:max-w-md md:max-w-2xl",
-                    p {
-                        "{msg}"
+                    class: "flex items-start p-5",
+                    img {
+                        class: "mr-2 h-8 w-8 rounded-full",
+                        src: "https://dummyimage.com/128x128/363536/ffffff&text={char}"
+                    }
+                    div {
+                        class: "flex rounded-b-xl rounded-tr-xl bg-slate-50 p-4 dark:bg-slate-800 sm:max-w-md md:max-w-2xl",
+                        p {
+                            "{msg}"
+                        }
                     }
                 }
             }
