963af5db275037628c151ab31f7d93325234dd3d
Feng Liang <feng.liang@kaust.edu.sa>
2023-11-07 19:10:08 +0800

    extract out MarkdownTextBox

--- src/components/chat/message_card.rs
+++ src/components/chat/message_card.rs
@@ -1,7 +1,7 @@
 use dioxus::prelude::*;
+use dioxus_markdown::Markdown;
 use transprompt::async_openai::types::Role;
 use transprompt::utils::llm::openai::ChatMsg;
-use dioxus_markdown::Markdown;
 
 #[derive(Props, PartialEq, Clone, Debug)]
 pub struct MessageCardProps {
@@ -19,14 +19,8 @@ pub fn MessageCard(cx: Scope<MessageCardProps>) -> Element {
                         class: "ml-2 h-8 w-8 rounded-full",
                         src: "https://dummyimage.com/128x128/354ea1/ffffff&text=S"
                     }
-                    div {
-                        class: "flex min-h-[85px] rounded-b-xl rounded-tl-xl bg-slate-50 p-4 dark:bg-slate-800 sm:min-h-0 sm:max-w-md md:max-w-2xl",
-                        article {
-                            class: "prose dark:prose-invert",
-                                Markdown {
-                                content: "{msg}",
-                            }
-                        }
+                    MarkdownTextBox {
+                        content: msg,
                     }
                 }
             },
@@ -40,18 +34,12 @@ pub fn MessageCard(cx: Scope<MessageCardProps>) -> Element {
                         class: "ml-2 h-8 w-8 rounded-full",
                         src: "https://dummyimage.com/128x128/354ea1/ffffff&text={char}"
                     }
-                    div {
-                        class: "flex min-h-[85px] rounded-b-xl rounded-tl-xl bg-slate-50 p-4 dark:bg-slate-800 sm:min-h-0 sm:max-w-md md:max-w-2xl",
-                        article {
-                            class: "prose lg:prose-xl",
-                                Markdown {
-                                content: "{msg}",
-                            }
-                        }
+                    MarkdownTextBox {
+                        content: msg,
                     }
                 }
             }
-        },
+        }
         Role::Assistant => {
             let name = chat_msg.msg.name.as_ref();
             let char = name.map(|name| name.chars().next().unwrap()).unwrap_or('A');
@@ -62,18 +50,32 @@ pub fn MessageCard(cx: Scope<MessageCardProps>) -> Element {
                         class: "mr-2 h-8 w-8 rounded-full",
                         src: "https://dummyimage.com/128x128/363536/ffffff&text={char}"
                     }
-                    div {
-                        class: "flex rounded-b-xl rounded-tr-xl bg-slate-50 p-4 dark:bg-slate-800 sm:max-w-md md:max-w-2xl",
-                        article {
-                            class: "prose lg:prose-xl",
-                                Markdown {
-                                content: "{msg}",
-                            }
-                        }
+                    MarkdownTextBox {
+                        content: msg,
                     }
                 }
             }
-        },
+        }
         Role::Function => unreachable!(),
+    }
+}
+
+#[derive(Props, PartialEq, Clone, Debug)]
+pub struct MarkdownTextBoxProps<'a> {
+    content: &'a str,
+}
+
+pub fn MarkdownTextBox<'a>(cx: Scope<'a, MarkdownTextBoxProps>) -> Element<'a> {
+    let msg = cx.props.content;
+    render! {
+        div {
+            class: "flex min-h-[85px] rounded-b-xl rounded-tl-xl bg-slate-50 px-4 dark:bg-slate-800 sm:min-h-0 sm:max-w-md md:max-w-2xl",
+            article {
+                class: "prose dark:prose-invert lg:prose-xl max-w-none",
+                    Markdown {
+                    content: "{msg}",
+                }
+            }
+        }
     }
 }
