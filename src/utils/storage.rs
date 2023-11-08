6779f557747b25a6104e466f5556e7dad9e16189
Feng Liang <feng.liang@kaust.edu.sa>
2023-11-08 16:07:55 +0800

    store Auth separately

--- src/utils/storage.rs
+++ src/utils/storage.rs
@@ -1,12 +1,10 @@
 use std::collections::HashMap;
 
-use gloo_storage::{LocalStorage, Storage};
 use serde::{Deserialize, Serialize};
 
 pub(crate) use schema::*;
 
 use crate::agents::{AgentConfig, AgentName};
-use crate::APP_NAME;
 use crate::chat::Chat;
 use crate::utils::auth::Auth;
 use crate::utils::customization::Customization;
@@ -29,38 +27,12 @@ pub struct StoredStates {
 
 impl StoredStates {
     pub fn get_or_init() -> Self {
-        let key = APP_NAME;
-
-        match LocalStorage::get::<RawStoredStates>(key) {
-            Ok(value) => value.into(),
-            Err(e) => {
-                log::error!("error: {}", e);
-                let (mut default_chat, name_to_configs) = Chat::default_chat_and_configs();
-                default_chat.topic = "Default Chat".to_string();
-                let stored_states = Self {
-                    run_count: 0,
-                    customization: Default::default(),
-                    name_to_configs,
-                    chats: vec![default_chat],
-                    auth: None,
-                    selected_service: None,
-                    openai_model: None,
-                };
-                let raw_stored_states = RawStoredStates::from(stored_states);
-                if let Err(e) = LocalStorage::set(key, &raw_stored_states) {
-                    log::error!("getting local storage error: {}", e);
-                }
-                raw_stored_states.into()
-            }
-        }
+        RawStoredStates::get_or_init()
     }
 
     pub fn save(&self) {
         let saved_storage: RawStoredStates = self.clone().into();
-        match LocalStorage::set(APP_NAME, saved_storage) {
-            Ok(_) => log::info!("Saved StoredStates"),
-            Err(e) => log::error!("Error when saving StoredStates: {}", e),
-        }
+        saved_storage.save();
     }
 }
 
--- src/utils/storage/schema.rs
+++ src/utils/storage/schema.rs
@@ -1,14 +1,18 @@
 use std::collections::HashMap;
 
+use gloo_storage::{LocalStorage, Storage};
 use serde::{Deserialize, Serialize};
 use transprompt::utils::llm::openai::ChatMsg;
 use uuid::Uuid;
 
 use crate::agents::{AgentConfig, AgentID, AgentName};
+use crate::APP_NAME;
+use crate::chat::Chat;
 use crate::utils::auth::Auth;
 use crate::utils::customization::Customization;
 use crate::utils::datetime::DatetimeString;
 use crate::utils::settings::{GPTService, OpenAIModel};
+use crate::utils::storage::StoredStates;
 
 pub(crate) type UUIDKey = String;
 pub(crate) type UUIDString = String;
@@ -31,6 +35,50 @@ pub(crate) struct RawStoredStates {
     pub selected_service: Option<GPTService>,
     #[serde(skip_serializing_if = "Option::is_none")]
     pub openai_model: Option<OpenAIModel>,
+}
+
+impl RawStoredStates {
+    fn auth_key() -> String {
+        format!("{}-auth", APP_NAME)
+    }
+    pub fn get_or_init() -> StoredStates {
+        match LocalStorage::get::<RawStoredStates>(APP_NAME) {
+            Ok(value) => value.into(),
+            Err(e) => {
+                log::error!("error: {}", e);
+                let (mut default_chat, name_to_configs) = Chat::default_chat_and_configs();
+                default_chat.topic = "Default Chat".to_string();
+                let mut stored_states = StoredStates {
+                    run_count: 0,
+                    customization: Default::default(),
+                    name_to_configs,
+                    chats: vec![default_chat],
+                    auth: None,
+                    selected_service: None,
+                    openai_model: None,
+                };
+                if let Ok(auth) = LocalStorage::get::<Auth>(Self::auth_key()) {
+                    stored_states.auth = Some(auth);
+                }
+                let raw_stored_states = RawStoredStates::from(stored_states.clone());
+                raw_stored_states.save();
+                stored_states
+            }
+        }
+    }
+
+    pub fn save(self) {
+        if self.auth.is_some() {
+            match LocalStorage::set(Self::auth_key(), self.auth.clone().unwrap()) {
+                Ok(_) => log::info!("Saved Auth"),
+                Err(e) => log::error!("Error when saving Auth: {}", e),
+            }
+        }
+        match LocalStorage::set(APP_NAME, self) {
+            Ok(_) => log::info!("Saved StoredStates"),
+            Err(e) => log::error!("Error when saving StoredStates: {}", e),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize)]
